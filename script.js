23c5cf105dfa68f9f41dc7c805128cf3c078a382
jooao2009 <joao.gomes.santos27@escola.pr.gov.br>
2025-09-04 09:05:17 -0300

    ok

---
+++ script.js
@@ -0,0 +1,18 @@
+const avanca = document.querySelectorAll('btn-proximo');
+const reiniciarBtn =document.getElementById('.btm-reniciar');
+
+avanca.forEach(button => {
+button.addEventListener('click',function(){
+    const atual = document.querySelector(.ativo);
+    const proximoPasso='passo-'+this,getAttribute('data-proximo')
+})
+
+
+
+
+}
+
+
+
+
+)
