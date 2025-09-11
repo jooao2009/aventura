8e338b5b9399aad918a976900d472a47e768e133
jooao2009 <joao.gomes.santos27@escola.pr.gov.br>
2025-09-11 09:06:23 -0300

    ok

--- script.js
+++ script.js
@@ -5,14 +5,33 @@ avanca.forEach(button => {
 button.addEventListener('click',function(){
     const atual = document.querySelector(.ativo);
     const proximoPasso='passo-'+this,getAttribute('data-proximo')
-})
+
+    atual.classlist.remove('ativo')
+    const proxmoelemento= document.getElementById(proximoPasso);
 
 
+if(proxmoelemento){
+   proxmoelemento.classList.add('ativo');
+  }else {
+console.error(`Elemento com ID "${porximoPasso}não encontrado,`)
+  }
+  
 
 
+  })
+});
+
+// Reinicia o jogo ao clicar no botão de reinício
+if (reiniciarBtn) {
+    reiniciarBtn.addEventListener('click', () => {
+        const atual = document.querySelector('.ativo');
+        atual.classList.remove('ativo');
+        document.getElementById('passo-0').classList.add('ativo');
+    });
 }
 
 
 
 
-)
+
+
